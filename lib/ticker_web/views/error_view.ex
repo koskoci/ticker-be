cd4f28e896ec7f04bc517985b65e164983f1fdd6
Balazs Koskoci <Balazs.Koskoci@logmein.com>
2020-01-16 15:00:18 +0100

    Render JSON errors

--- lib/ticker_web/views/error_view.ex
+++ lib/ticker_web/views/error_view.ex
@@ -1,8 +1,15 @@
 defmodule TickerWeb.ErrorView do
   use TickerWeb, :view
 
+  def render("400.json", _assigns) do
+    %{errors: %{message: "Bad request"}}
+  end
 
-  def template_not_found(template, _assigns) do
-    %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
+  def render("404.json", _assigns) do
+    %{errors: %{message: "Not Found"}}
+  end
+
+  def render("500.json", _assigns) do
+    %{errors: %{message: "Server Error"}}
   end
 end
